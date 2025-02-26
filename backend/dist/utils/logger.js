538b71b2064a8ceec1ab611459dbfe5c708e1efe
ayushgupta9906 <ayush150152@gmail.com>
2025-02-26 21:17:33 +0530

    deploy

--- backend/dist/utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logError = exports.logInfo = void 0;
-const logInfo = (message) => console.log(`ℹ️ ${message}`);
-exports.logInfo = logInfo;
-const logError = (message) => console.error(`❌ ${message}`);
-exports.logError = logError;
