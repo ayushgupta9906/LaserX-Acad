d81e2ae3bab73239784b7742587e4de59db213b0
ayushgupta9906 <ayush150152@gmail.com>
2025-02-26 14:32:13 +0530

    commonjs

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -11,15 +11,19 @@ const db_1 = __importDefault(require("./config/db"));
 const routes_1 = __importDefault(require("./routes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+// Database Connection
+(0, db_1.default)();
 // Middleware
 app.use((0, cors_1.default)({ origin: process.env.CLIENT_URL || "http://localhost:3000", credentials: true }));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, morgan_1.default)("dev"));
-// Database Connection
-(0, db_1.default)();
-// Routes
-app.use("/", routes_1.default); // API should have a prefix
+// Default Route
+app.get("/", (req, res) => {
+    res.send("🚀 Server is running!");
+});
+// API Routes
+app.use("/", routes_1.default); // Added "/api" as a prefix for clarity
 // Server Start
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
